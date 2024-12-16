c260119a77c38130ba8a177dc034d09f971faab8
JaagupPr <158137390+JaagupPr@users.noreply.github.com>
2024-12-16 06:37:56 +0200

    Added frontend vue and sequalize files

---
+++ Frontend/frontend/src/components/ProductsTable.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name: "ProductsTable",
+    props: {
+        items: Array
+    }
+}
+</script>
+
+<template>
+    <table class="table table-striped">
+        <thead class="table-dark">
+            <tr>
+                <th>ProductName</th>
+                <th>ReleaseDateEU</th>
+                <th>ProductStockQuantity</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in items" :key="item.ProductID">
+                <td>{{ item.GameName }}</td>
+                <td>{{ item.ReleaseDateEU }}</td>
+                <td>{{ item.ProductStockQuantity }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<style scoped>
+</style>
---
+++ Frontend/frontend/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script>
+import ProductsTable from '@/components/ProductsTable.vue'
+export default {
+  components: { 
+    ProductsTable
+  },
+  data() { 
+    return {
+    allProducts: []
+  }
+},
+  async Created() {
+    this.allProducts = await (await fetch('http://localhost:8080/products')).json()
+  }
+}
+</script>
+
+<template>
+  <main>
+    <ProductsTable :items="allProducts
+    
+    "/>
+  </main>
+</template>
