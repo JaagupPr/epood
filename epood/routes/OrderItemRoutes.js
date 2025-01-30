dd42b5732a4ba14206c99171fbe53688f0b14cd3
JaagupPr <158137390+JaagupPr@users.noreply.github.com>
2025-01-30 10:09:50 +0200

    routes for order item

---
+++ epood/routes/OrderItemRoutes.js
@@ -0,0 +1,13 @@
+const OrderItemsController = require('../controllers/OrderItemsController');
+
+module.exports = (app) => {
+    app.route("/orderitems")
+        .get(OrderItemsController.getAll)
+        .post(OrderItemsController.create)
+
+    app.route("/orderitems/:id")
+        .get(OrderItemsController.getById)
+        .put(OrderItemsController.updateById)
+        .delete(OrderItemsController.deleteById)
+        
+}
